51b8a2e6c58e7fc33df2fd130b6134d837b48701
Ario Setiawan <consciousness.dev@gmail.com>
2019-03-08 20:53:57 +0700

    hosting to github pages

---
+++ devsites/portfolio/reacthost/precache-manifest.4a5135be209a4b06a2bc42a7cd0d06d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "76fd0901c8fb1d32ba6e",
+    "url": "/devsites/portfolio/reacthost/static/css/main.2da45e0e.chunk.css"
+  },
+  {
+    "revision": "76fd0901c8fb1d32ba6e",
+    "url": "/devsites/portfolio/reacthost/static/js/main.1e96a4ec.chunk.js"
+  },
+  {
+    "revision": "a8bb8bece55f82baafbf",
+    "url": "/devsites/portfolio/reacthost/static/js/runtime~main.4e2319cd.js"
+  },
+  {
+    "revision": "e53193c57cbe44c02664",
+    "url": "/devsites/portfolio/reacthost/static/js/2.5a45f50a.chunk.js"
+  },
+  {
+    "revision": "ff61d7ecb029ee109671719dd0291f00",
+    "url": "/devsites/portfolio/reacthost/index.html"
+  }
+];
